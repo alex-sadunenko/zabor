a4275c68d2ef3399bc599e8f591a96aff9aec5e1
alexandr_sadunenko <a.sadunenko@gmail.com>
2020-09-12 10:31:45 +0300

    final

--- Zabor/View/ReportViewController.swift
+++ Zabor/View/ReportViewController.swift
@@ -22,15 +22,10 @@ class ReportViewController: UIViewController {
     let datePicker = UIDatePicker()
     let userDefaults = UserDefaults.standard
     var filteredProduct = [Product]()
-    //    var searchBarIsEmpty: Bool {
-    //        guard let text = searchController.searchBar.text else { return false }
-//        return text.isEmpty
-//    }
-//    var isFiltering: Bool { return searchController.isActive && !searchBarIsEmpty }
+    let codeTextView = UITextField()
 
     //MARK: - IBOutlet
     @IBOutlet weak var segmentControl: UISegmentedControl!
-    @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var checkLabel: UILabel! {
         didSet {
             checkLabel.isHidden = true
@@ -56,24 +51,41 @@ class ReportViewController: UIViewController {
         
         configureSearchController()
         configureTextField()
-
+        
     }
     
     @IBAction func segmentAction(_ sender: UISegmentedControl) {
-        if sender.selectedSegmentIndex == 2 {
-            //navigationItem.searchController = nil
-
-            checkLabel.isHidden = false
-            switchControl.isHidden = false
-            dateTextField.isHidden = true
-        } else {
+        
+        cancelAction()
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            searchController.searchBar.placeholder = "Выберите дату"
             checkLabel.isHidden = true
             switchControl.isHidden = true
-            dateTextField.isHidden = false
+            filteredProduct.removeAll()
+            tableView.reloadData()
+        case 1:
+            searchController.searchBar.placeholder = "Укажите объект"
+            checkLabel.isHidden = true
+            switchControl.isHidden = true
+            filteredProduct.removeAll()
+            tableView.reloadData()
+        case 2:
+            searchController.searchBar.placeholder = ""
+            checkLabel.isHidden = false
+            switchControl.isHidden = false
+            switchAction(switchControl)
+        default: return
         }
+
     }
     
     @IBAction func switchAction(_ sender: UISwitch) {
+        filteredProduct = productArray.filter({ (products: Product) -> Bool in
+            return products.isCheck == switchControl.isOn
+        })
+        tableView.reloadData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -99,13 +111,14 @@ class ReportViewController: UIViewController {
     func configureSearchController() {
         searchController.searchResultsUpdater = self
         searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search"
+        searchController.searchBar.placeholder = "Выберите дату"
+        searchController.searchBar.tintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         navigationItem.searchController = searchController
         definesPresentationContext = true
     }
     
     func configureTextField() {
-        dateTextField.inputView = datePicker
+        codeTextView.inputView = datePicker
         datePicker.datePickerMode = .date
         let localeID = Locale.preferredLanguages.first
         datePicker.locale = Locale(identifier: localeID!)
@@ -113,14 +126,17 @@ class ReportViewController: UIViewController {
         let tooldar = UIToolbar()
         tooldar.sizeToFit()
         let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: nil, action: #selector(cancelAction))
+        cancelButton.tintColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
+        doneButton.tintColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
         tooldar.setItems([cancelButton,flexSpace,doneButton], animated: true)
-        dateTextField.inputAccessoryView = tooldar
+        codeTextView.inputAccessoryView = tooldar
+        view.addSubview(codeTextView)
     }
     
     @objc func cancelAction() {
-        dateTextField.text = ""
+        codeTextView.text = ""
         searchController.searchBar.text = ""
         view.endEditing(true)
     }
@@ -133,7 +149,6 @@ class ReportViewController: UIViewController {
     func getDateFromPicker() {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
-        dateTextField.text = formatter.string(from: datePicker.date)
         searchController.searchBar.text = formatter.string(from: datePicker.date)
     }
 
@@ -146,6 +161,7 @@ extension ReportViewController: UISearchResultsUpdating {
         let searchText = searchController.searchBar.text!
         switch segmentControl.selectedSegmentIndex {
         case 0:
+            codeTextView.becomeFirstResponder()
             filteredProduct = productArray.filter({ (products: Product) -> Bool in
                 return products.date.lowercased().contains(searchText.lowercased())
             })
@@ -155,7 +171,7 @@ extension ReportViewController: UISearchResultsUpdating {
             })
         case 2:
             filteredProduct = productArray.filter({ (products: Product) -> Bool in
-                return products.description.lowercased().contains(searchText.lowercased())
+                return products.isCheck == switchControl.isOn
             })
         default:
             return
@@ -189,10 +205,10 @@ extension ReportViewController: UITableViewDataSource {
             cell.nameLabel.text = filteredProduct[indexPath.row].date
             cell.descriptionLabel.text = filteredProduct[indexPath.row].description
         case 1:
-            cell.nameLabel.text = filteredProduct[indexPath.row].date
+            cell.nameLabel.text = searchController.searchBar.text
             cell.descriptionLabel.text = filteredProduct[indexPath.row].description
         case 2:
-            cell.nameLabel.text = searchController.searchBar.text
+            cell.nameLabel.text = switchControl.isOn ? "На учете" : "Без учета"
             cell.descriptionLabel.text = filteredProduct[indexPath.row].description
         default:
             cell.nameLabel.text = ""
